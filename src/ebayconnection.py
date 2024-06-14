c99bf7f549d0c08df03f2d23954bc5ee0190c2dd
sachins301 <sachin.sudheer301@gmail.com>
2024-06-13 18:40:43 -0600

    Update ebayconnection.py

--- src/ebayconnection.py
+++ src/ebayconnection.py
@@ -41,3 +41,21 @@ if response.status_code == 200:
 else:
     print("Error:", response.status_code)
 
+import pandas as pd
+from pandas import json_normalize
+import json
+
+json_path = "../resources/ebaydata.json"
+
+with open(json_path, "r") as file:
+    json_data = json.load(file)
+
+if 'itemSummaries' in json_data:
+    item_summaries = json_data['itemSummaries']
+    print(item_summaries)
+else:
+    print("Key 'itemSummaries' not found in the JSON data.")
+
+df = json_normalize(item_summaries)
+
+df.to_csv('../resources/ebaydata.csv', index=False)
